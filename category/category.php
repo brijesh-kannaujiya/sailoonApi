e2955b42e42d2142c4c2c9e28321442e7f7f88f3
brijeshofficial <bk091192@gmail.com>
2024-03-02 10:09:10 +0530

    update

--- category/category.php
+++ category/category.php
@@ -1,7 +1,8 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
 include 'categories.php';
 // Handle requests
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
